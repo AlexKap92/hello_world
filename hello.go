d62b90ea03cfd5c27941e8dbb81606d28461c5e1
AlexKap92 <alexcap92@gmail.com>
2024-04-09 20:02:48 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -4,5 +4,31 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Привет, Мир! Привет, Кодбай!")
+	var income, food, transport, housing float64
+
+	fmt.Print("Введите ваш месячный доход: ")
+	fmt.Scan(&income)
+
+	fmt.Print("Введите расходы на еду: ")
+	fmt.Scan(&food)
+
+	fmt.Print("Введите расходы на транспорт: ")
+	fmt.Scan(&transport)
+
+	fmt.Print("Введите расходы на жилье: ")
+	fmt.Scan(&housing)
+
+	remainingIncome := income - (food + transport + housing)
+
+	fmt.Println("Чистый доход: ", remainingIncome)
+
+	if food/income >= 0.3 {
+		fmt.Println("Внимание: расходы на еду составляют больше 30% от вашего дохода. Рекомендуется снизить траты в этой категории.")
+	}
+	if transport/income >= 0.3 {
+		fmt.Println("Внимание: расходы на транспорт составляют больше 30% от вашего дохода. Рекомендуется снизить траты в этой категории.")
+	}
+	if housing/income >= 0.3 {
+		fmt.Println("Внимание: расходы на жильё составляют больше 30% от вашего дохода. Рекомендуется снизить траты в этой категории.")
+	}
 }
